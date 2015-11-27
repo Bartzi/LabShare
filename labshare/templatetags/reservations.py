0755e6747eea89ceeae5dff46757d6ae9f483d84
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2015-11-27 15:48:46 +0100

    show position of a user in waiting queue

--- labshare/templatetags/reservations.py
+++ labshare/templatetags/reservations.py
@@ -9,9 +9,21 @@ def current_reservation(gpu):
         return ""
     return reservations.order_by("time_reserved").first().user
 
+
 @register.filter
 def next_reservation(gpu):
     reservations = gpu.reservations.all()
     if len(reservations) <= 1:
         return ""
     return reservations.order_by("time_reserved").all()[1].user
+
+
+@register.filter
+def queue_position(gpu, user):
+    reservations = gpu.reservations.order_by("time_reserved").all()
+    user_reservations = reservations.filter(user__id=user.id)
+    if len(user_reservations) < 1:
+        return None
+    else:
+        return reservations.filter(
+            time_reserved__lt=user_reservations.first().time_reserved).count()
