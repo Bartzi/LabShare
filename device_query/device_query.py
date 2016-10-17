0fefaca8552cb08b791d537774ae9421843ca97b
Dimitri Korsch <korschdima@gmail.com>
2016-10-17 08:56:27 +0200

    added process info parsing

--- device_query/device_query.py
+++ device_query/device_query.py
@@ -6,24 +6,36 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class DeviceQueryHandler(BaseHTTPRequestHandler):
 
+    def __parse_memory(self, memory_usage):
+        return {
+            "total": memory_usage.find("total").text,
+            "used": memory_usage.find("used").text,
+            "free": memory_usage.find("free").text,
+        }
+
+    def __parse_processes(self, procs):
+        processes = []
+        if procs.text == "N/A": return processes
+
+        for process in procs.iter("process_info"):
+            processes.append({
+                "pid": process.find("pid").text,
+                "process_name": process.find("process_name").text,
+                "used_memory": process.find("used_memory").text,
+            }   )
+        return processes
+
     def parse_nvidia_xml(self, xml):
         gpu_data = []
         root = ET.fromstring(xml)
         for gpu in root.iter('gpu'):
-            current_gpu_data = {
+            gpu_data.append({
                 "name": gpu.find("product_name").text,
                 "uuid": gpu.find("uuid").text,
-            }
+                "memory": self.__parse_memory(gpu.find("fb_memory_usage")),
+                "processes": self.__parse_processes(gpu.find("processes")),
+            })
 
-            memory_usage = gpu.find("fb_memory_usage")
-            memory = {
-                "total": memory_usage.find("total").text,
-                "used": memory_usage.find("used").text,
-                "free": memory_usage.find("free").text,
-            }
-
-            current_gpu_data["memory"] = memory
-            gpu_data.append(current_gpu_data)
         return gpu_data
 
     def do_GET(self):
@@ -35,7 +47,7 @@ class DeviceQueryHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(bytes(json.dumps(gpu_data, indent=4), 'utf-8'))
         except Exception as e:
-            self.send_error(500)
+            self.send_error(500, explain = str(e))
 
 
 if __name__ == "__main__":
