97e6defc7517d626885b50e336de1d78815dda8a
Christian Bartz <christian.bartz@student.hpi.uni-potsdam.de>
2016-10-26 17:12:34 +0200

    fix migration

--- labshare/migrations/0013_initial_groups.py
+++ labshare/migrations/0013_initial_groups.py
@@ -3,12 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib.auth.models import Group
 from django.db import migrations
-from guardian.shortcuts import assign_perm
 
 
 def initial_data(apps, schema_editor):
     staff = Group.objects.create(name="Staff")
-    assign_perm('labshare.use_device', staff)
     staff.save()
 
 
