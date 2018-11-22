9dbdedb9b981c0af3af2dd34497d3fcc6b545ed4
Christian Bartz <christian.bartz@student.hpi.uni-potsdam.de>
2018-11-22 11:05:39 +0100

    improve handling of changing email addresses in ldap backend

--- labshare/backends/authentication/ldap.py
+++ labshare/backends/authentication/ldap.py
@@ -10,11 +10,21 @@ class LDAPBackend(DjangoLDAPBackend):
         if user is None:
             return None
 
-        # because we can have multiple email addresses per user we need to store
+        # because we can have multiple email addresses per user we need to store them in a different object
         email_addresses = ldap_user.attrs[ldap_user.settings.USER_ATTR_MAP['email']]
-        if len(email_addresses) > 1 and user.email_addresses.count() == 0:
+        if len(email_addresses) != user.email_addresses.count() + 1:
+            # if the first address changed in LDAP, we also have to change it here
+            if user.email != email_addresses[0]:
+                user.email = email_addresses[0]
+                user.save()
+
+            # first: delete all current addresses and then start over
+            current_addresses = EmailAddress.objects.filter(user=user)
+            for address in current_addresses:
+                address.delete()
+
+            # second: create extra email address objects for user
             for address in email_addresses[1:]:
-                # create extra email address objects for user
                 email = EmailAddress.objects.create(user=user, email=address)
                 email.save()
 
