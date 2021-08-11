712ad8d4be704e7f58b66781aa3771b3e68cacbf
Christian Bartz <christian.bartz@hpi.de>
2021-08-11 16:38:34 +0200

    delete old unused code

--- labshare/forms.py
+++ labshare/forms.py
@@ -2,19 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 from django.forms import SelectMultiple, Select
 
-from .utils import get_devices
-
-
-class DeviceSelectForm(forms.Form):
-    device = forms.ChoiceField(choices=get_devices)
-    gpu = forms.CharField()
-    next_available_spot = forms.HiddenInput()
-
-    def __init__(self, *args, **kwargs):
-        devices = kwargs.pop('devices')
-        super(DeviceSelectForm, self).__init__(*args, **kwargs)
-        self.fields['device'].choices = [(device.name, device.name) for device in devices]
-
 
 class MessageForm(forms.Form):
     message_all_users = forms.BooleanField(required=False)
